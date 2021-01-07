3fdd42e014db2f8fe04fcafe6b62d3b1d1ed3124
tangpeng <1045511079@qq.com>
2021-01-07 11:14:43 +0800

    feat(main):查询黑名单进行登录限制

--- blog-main-module-service/src/main/java/com/tpblog/main/controller/oam/BlackListMainController.java
+++ blog-main-module-service/src/main/java/com/tpblog/main/controller/oam/BlackListMainController.java
@@ -1,25 +1,42 @@
 package com.tpblog.main.controller.oam;
 
+import com.tpblog.main.api.response.Result;
+import com.tpblog.main.api.response.ResultUtil;
 import com.tpblog.main.service.oam.BlackListMainService;
+import com.tpblog.main.service.user.UserService;
+import com.tpblog.main.utils.LoginCheck;
 import com.tpblog.oam.api.entity.BlackList;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("blacklist")
+@Slf4j
 public class BlackListMainController {
 
     @Autowired
     private BlackListMainService blackListMainService;
 
-    @GetMapping("find")
-    public List<BlackList> findBlackListAll(){
-        List<BlackList> blackListAll = blackListMainService.findBlackListAll();
+    @Autowired
+    private LoginCheck loginCheck;
 
-        return blackListAll;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("find")
+    public Result findBlackListAll(HttpServletRequest request){
+        Result login = loginCheck.isLogin(request);
+        if (login!=null) {
+            return login;
+        }
+        List<BlackList> blackListAll = blackListMainService.findBlackListAll();
+        return ResultUtil.success(blackListAll);
     }
 }
